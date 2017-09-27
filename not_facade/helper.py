122e186e0e8993534bf984d8801441162845a8e7
Alen Thomas <alenthomas9@gmail.com>
2017-09-27 13:42:26 +0530

    Add helper functions for FB Graph API, import keys from OS ENV

---
+++ not_facade/helper.py
@@ -0,0 +1,66 @@
+import json
+from urllib import request
+
+from keys import FB_FACADE_TOKEN
+
+# Typewritersvoice
+# PAGE URL
+url0 = "https://graph.facebook.com/v2.10/{}?access_token={}"
+
+# PAGE ALBUMS
+url1 = "https://graph.facebook.com/v2.10/{}/albums?access_token={}"
+
+# PAGE TIMELINE PHOTOS
+url2 = "https://graph.facebook.com/v2.10/{}/photos?access_token={}"
+
+# link of media content
+url3 = "https://graph.facebook.com/v2.10/{}?access_token={}&fields=source"
+
+
+def get_page(page_name):
+    api = url0.format(page_name, FB_FACADE_TOKEN)
+    response = request.urlopen(api)
+    result = json.load(response)
+    try:
+        return {'id':result['id'], 'name':result['name']}
+    except KeyError as err:
+        raise(err)
+
+def get_albums(page_id):
+    api = url1.format(page_id, FB_FACADE_TOKEN)
+    response = request.urlopen(api)
+    result = json.load(response)
+    timeline_album = {}
+    try:
+        for album in result['data']:
+            if(album['name'] == 'Timeline Photos'):
+                timeline_album['name'] = album['name']
+                timeline_album['id'] = album['id']
+                break
+        return timeline_album
+    except KeyError as err:
+        raise(err)
+
+def get_images(album_id):
+    api = url2.format(album_id, FB_FACADE_TOKEN)
+    response = request.urlopen(api)
+    result = json.load(response)
+    img_ids = []
+    paging = None
+    try:
+        paging = result['paging']
+        for img in result['data']:
+            img_ids.append(img['id'])
+        return {'data':img_ids, 'paging':paging}
+    except KeyError as err:
+        raise
+
+def get_img_src(img_id):
+    api = url3.format(img_id, FB_FACADE_TOKEN)
+    response = request.urlopen(api)
+    result = json.load(response)
+    try:
+        src = result['source']
+        return {'src':src}
+    except KeyError as err:
+        raise(err)
---
+++ not_facade/keys.py
@@ -0,0 +1,4 @@
+import os
+
+FACADE_FLASK_SECRET = os.environ.get('FACADE_FLASK_SECRET')
+FB_FACADE_TOKEN = os.environ.get('FB_FACADE_TOKEN')
