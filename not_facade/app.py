6196431c35ccfad4390169353e3933c2c1dc947c
Alen Thomas <alenthomas9@gmail.com>
2017-09-27 15:10:38 +0530

    Add post handler in home

--- not_facade/app.py
+++ not_facade/app.py
@@ -1,6 +1,8 @@
 from flask import Flask
 from flask import render_template as rt
+from flask import request as req
 
+from sanitation import check_form_data
 from keys import FACADE_FLASK_SECRET
 
 app = Flask(__name__)
@@ -9,6 +11,13 @@ app.config.update(dict(
     SECRET_KEY=FACADE_FLASK_SECRET
 ))
 
-@app.route("/")
+@app.route("/", methods=['GET', 'POST'])
 def home():
-    return rt('home.html')
+    if req.method == 'GET':
+        return rt('home.html')
+    elif req.method == 'POST':
+        if check_form_data(['page_name'], req.form):
+            page = req.form['page_name']
+            return rt('home.html', post=True, page_name=page)
+        else:
+            return rt('home.html')
