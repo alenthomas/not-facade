87ce48cd5f559504fd11e6d1fb346216a5dcbd50
Alen Thomas <alenthomas9@gmail.com>
2017-09-27 16:14:34 +0530

    Display first 10 posts of any public page

--- not_facade/app.py
+++ not_facade/app.py
@@ -4,6 +4,7 @@ from flask import request as req
 
 from sanitation import check_form_data
 from keys import FACADE_FLASK_SECRET
+from helper import get_page, get_albums, get_images, get_img_src
 
 app = Flask(__name__)
 app.config.from_object(__name__)
@@ -11,13 +12,18 @@ app.config.update(dict(
     SECRET_KEY=FACADE_FLASK_SECRET
 ))
 
-@app.route("/", methods=['GET', 'POST'])
+@app.route("/",)
 def home():
-    if req.method == 'GET':
-        return rt('home.html')
-    elif req.method == 'POST':
-        if check_form_data(['page_name'], req.form):
-            page = req.form['page_name']
-            return rt('home.html', post=True, page_name=page)
-        else:
-            return rt('home.html')
+    return rt('home.html')
+
+@app.route("/page", methods=['POST'])
+def page():
+    if check_form_data(['page_name'], req.form):
+        page_name = req.form['page_name']
+        page = get_page(page_name)
+        album = get_albums(page['id'])
+        image_ids = get_images(album['id'])['data']
+        links = []
+        for i in image_ids:
+            links.append(get_img_src(i))
+        return rt('page.html', page_name=page['name'], data=links)
