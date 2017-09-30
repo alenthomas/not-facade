1eb415c0c69373ae325a9e64e3fc40caaf8caa9b
Alen Thomas <alenthomas9@gmail.com>
2017-09-30 13:23:51 +0530

    Return json response; Add method to get image link

--- not_facade/app.py
+++ not_facade/app.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from flask import render_template as rt
 from flask import request as req
+from flask import jsonify
 
 from sanitation import check_form_data
 from keys import FACADE_FLASK_SECRET
@@ -21,6 +22,7 @@ def page():
     if check_form_data(['page_name'], req.form):
         page_name = req.form['page_name']
         page = get_page(page_name)
+        print("\npageID", page['id'], "\n")
         #album = get_albums(page['id'])
         #image_ids = get_images(album['id'])['data']
         links = []
@@ -32,7 +34,7 @@ def page():
 def get_page_albums():
     if req.args.get("pageid"):
         album = get_albums(req.args.get('pageid'))
-        return album
+        return jsonify(album)
     else:
         return "Invalid page id"
 
@@ -40,6 +42,11 @@ def get_page_albums():
 def get_album_images():
     if req.args.get("albumid"):
         image_ids = get_images(req.args.get('albumid'))['data']
-        return image_ids
+        return jsonify(image_ids)
     else:
         return "Invalid Album Id"
+
+@app.route("/image/<imgid>")
+def get_img_by_id(imgid):
+    link = get_img_src(imgid)
+    return jsonify(link)
